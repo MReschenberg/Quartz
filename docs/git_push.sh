5aa2dc9430d096a83f19e54ea7989026e4f7e7d3
Morgan Rae Reschenberg <mreschenberg@berkeley.edu>
2025-12-11 14:16:10 -0800

    Quartz sync: Dec 11, 2025, 2:16 PM

---
+++ docs/git_push.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+# Date in format Day-Month-Year
+date=$(date +"%Y-%m-%d %T")
+
+# Commit message
+message="Commit for $date"
+git add -A
+git commit -m"${message}"
+status="$(git status --branch --porcelain)"
+echo $status >> ~/cron_echo.txt
+if [ "$status" == "## master...origin/master" ]; then
+  echo "IT IS CLEAN" >> ~/cron_echo.txt
+else
+  echo "There is stuff to push" >> ~/cron_echo.txt
+  git push
+fi
