8c7521c7d0fa4adf137587a3073b5fb9d681a6cf
Morgan Rae Reschenberg <mreschenberg@berkeley.edu>
2025-12-11 16:10:23 -0800

    Quartz sync: Dec 11, 2025, 4:10 PM

--- publish.sh
+++ publish.sh
@@ -1,3 +1,1 @@
-npx quartz build --directory=/Users/morganraereschenberg/Library/Mobile\ Documents/iCloud\~md\~obsidian/Documents --o docs
-
-npx quartz sync --no-pull
+npx quartz build --directory=/Users/morganraereschenberg/Library/Mobile\ Documents/iCloud\~md\~obsidian/Documents --o docs && npx quartz sync --no-pull
